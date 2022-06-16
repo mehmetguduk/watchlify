0661c2750c120292212e3db12dcaef1dbabec51b
mehmetguduk <mehmetguduk98@gmail.com>
2022-06-16 16:08:15 +0300

    master

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,31 +3,32 @@ import getSearch from "../utils/getSearch";
 
 export default function Search({ myList, setMyList }) {
 
+
     const [searchValue, setSearchValue] = React.useState("");
-    const [searchElements, setSearchElements] = React.useState([]);
     const [searchResponse, setSearchResponse] = React.useState([]);
 
     function handleSearch() {
         getSearch(searchValue).then(response => {
             setSearchResponse(response)
-            setSearchElements(response.map(show => {
-                return (
-                    <li id={show.show.id} key={show.show.id} onClick={handleShowClick}>
-                        {show.show.name}
-                    </li>
-                )
-            }))
         })
     }
 
     function handleShowClick(event) {
-        // IN THIS LINE searchResponse RETURNING ME AN EMPTY ARRAY. WHY?
-        // I CAN'T GET THE LASTEST UPDATE OF searchResponse IN THIS FUNCTION.
-        const element = searchResponse.filter(show => {
-            return (show.show.id === Number(event.target.id))
-        })[0]
-        // OUTPUT : []
-        console.log(element)
+        const showId = Number(event.target.id);
+        const show = searchResponse.find(show => show.show.id === showId);
+        setMyList(prevMyList => {
+
+            console.log(prevMyList) // [{show: {id: 1, name: "test"}, ...}]
+            console.log(show) // {show: {id: 1, name: "test"}}
+            console.log(prevMyList.includes(show)) // false
+
+            if (prevMyList.includes(show)) {
+                return prevMyList.filter(show => show.show.id !== showId)
+            }
+            else {
+                return [...prevMyList, show]
+            }
+        });
     }
 
     return (
@@ -39,12 +40,26 @@ export default function Search({ myList, setMyList }) {
                 onChange={(e) => setSearchValue(e.target.value)}
                 value={searchValue}
             />
-            <button
-                onClick={handleSearch}
-            >Search</button>
+            <button onClick={handleSearch}>Search</button>
 
             <ul className="show-results">
-                {searchElements}
+                {searchResponse.map(show => {
+                    if (myList.includes(show)) {
+                        return (
+                            <li id={show.show.id} key={show.show.id} onClick={handleShowClick} style={{ color: "#FF4500" }}>
+                                {show.show.name}
+                            </li>
+                        )
+                    }
+                    else {
+
+                        return (
+                            <li id={show.show.id} key={show.show.id} onClick={handleShowClick}>
+                                {show.show.name}
+                            </li>
+                        )
+                    }
+                })}
             </ul>
         </div>
     )
